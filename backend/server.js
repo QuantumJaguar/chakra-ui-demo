2cf246fa7d98fa318522d4a8c52df0a23d92098c
QuantumJaguar <thomaslong161@gmail.com>
2024-03-07 16:50:12 +0530

    updated

---
+++ backend/server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+const fs = require("fs");
+const cors = require("cors");
+app.use(cors());
+
+const PORT = 3000;
+
+app.get("/tasks", (req, res) => {
+  fs.readFile("db.json", "utf8", (err, data) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send("Internal Server Error");
+      return;
+    }
+    res.json(JSON.parse(data));
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
