7fa71beadf243b16e7356e285cb54124970fd4e9
QuantumJaguar <thomaslong161@gmail.com>
2024-03-08 17:17:02 +0530

    updated

--- api/index.js
+++ api/index.js
@@ -1,32 +1,30 @@
 const express = require("express");
 const app = express();
-const fs = require("fs");
+const fs = require("fs").promises;
 const cors = require("cors");
-app.use(express.json());
 
+app.use(
+  cors({
+    credentials: true,
+    origin: "http://127.0.0.1:5173",
+  })
+);
 const PORT = 3000;
 
-app.get("/api/", cors(), (req, res) => {
+app.get("/api/", (req, res) => {
   res.send("Express on Vercel");
 });
 // Define a route to serve JSON data from db.json
 
-app.get("/api/tasks", cors(), (req, res) => {
-  fs.readFile("db.json", "utf8", (err, data) => {
-    if (err) {
-      console.error("Error reading db.json:", err.message);
-      return res.status(500).json({ error: "Internal Server Error" });
-    }
-
-    try {
-      const jsonData = JSON.parse(data);
-
-      res.status(200).json(jsonData);
-    } catch (error) {
-      console.error("Error parsing JSON:", error.message);
-      res.status(500).json({ error: "JSON Parsing Error" });
-    }
-  });
+app.get("/api/tasks", cors(), async (req, res) => {
+  try {
+    const filePath = "db.json"; // Adjust the file path as needed
+    const contents = await fs.readFile(filePath, "utf-8");
+    res.send(contents);
+  } catch (error) {
+    console.error("Error:", error);
+    res.status(500).send("Internal Server Error");
+  }
 });
 
 app.listen(PORT, () => {
