66d2b8514a9e295edb5796b0d5b19a065374f7d7
QuantumJaguar <thomaslong161@gmail.com>
2024-03-08 17:07:15 +0530

    updated

--- api/index.js
+++ api/index.js
@@ -2,26 +2,16 @@ const express = require("express");
 const app = express();
 const fs = require("fs");
 const cors = require("cors");
+app.use(express.json());
 
-// Apply CORS configuration for the entire application
-app.use(
-  cors({
-    credentials: true,
-    origin: process.env.CLIENT_URL,
-  })
-);
 const PORT = 3000;
 
-app.get("/api/", (req, res) => {
+app.get("/api/", cors(), (req, res) => {
   res.send("Express on Vercel");
 });
 // Define a route to serve JSON data from db.json
 
-app.get("/api/tasks", (req, res) => {
-  // Set CORS headers
-  res.setHeader("Access-Control-Allow-Origin", "http://127.0.0.1:5173");
-  res.setHeader("Access-Control-Allow-Credentials", "true");
-
+app.get("/api/tasks", cors(), (req, res) => {
   fs.readFile("db.json", "utf8", (err, data) => {
     if (err) {
       console.error("Error reading db.json:", err.message);
@@ -30,8 +20,8 @@ app.get("/api/tasks", (req, res) => {
 
     try {
       const jsonData = JSON.parse(data);
-      res.setHeader("Content-Type", "application/json");
-      res.json(jsonData);
+
+      res.status(200).json(jsonData);
     } catch (error) {
       console.error("Error parsing JSON:", error.message);
       res.status(500).json({ error: "JSON Parsing Error" });
