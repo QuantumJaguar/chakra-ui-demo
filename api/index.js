30c4336c7fa44d76186332e223ef72d6791b603b
QuantumJaguar <thomaslong161@gmail.com>
2024-03-08 19:16:46 +0530

    updated

--- api/index.js
+++ api/index.js
@@ -1,26 +1,22 @@
 const express = require("express");
 const app = express();
-const fs = require("fs").promises;
+const fs = require("fs").promises; // Use fs.promises for asynchronous file operations
 const cors = require("cors");
+const path = require("path"); // Import the 'path' module
 
-app.use(
-  cors({
-    credentials: true,
-    origin: "http://127.0.0.1:5173",
-  })
-);
+app.use(cors());
+
 const PORT = 3000;
 
 app.get("/api/", (req, res) => {
   res.send("Express on Vercel");
 });
-// Define a route to serve JSON data from db.json
 
 app.get("/api/tasks", cors(), async (req, res) => {
   try {
-    const filePath = "db.json"; // Adjust the file path as needed
+    const filePath = path.join(process.cwd(), "public/db.json"); // Adjust the file path as needed
     const contents = await fs.readFile(filePath, "utf-8");
-    res.send(contents);
+    res.json(JSON.parse(contents)); // Assuming db.json contains valid JSON
   } catch (error) {
     console.error("Error:", error);
     res.status(500).send("Internal Server Error");
