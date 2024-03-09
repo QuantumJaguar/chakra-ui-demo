1163bb8de9eb8593725876b8bd8a2fd8c0753be8
QuantumJaguar <thomaslong161@gmail.com>
2024-03-09 08:08:07 +0530

    updated

--- api/index.js
+++ api/index.js
@@ -3,16 +3,14 @@ const fs = require("fs").promises;
 const cors = require("cors"); // Import the cors middleware
 const app = express();
 const port = process.env.PORT || 3000;
-
-// Use cors middleware
-app.use(cors());
+const jsonData = require("./db.json");
 
 // Define a route to serve JSON data from db.json
 app.get("/api/tasks", async (req, res) => {
   try {
-    // Read the content of db.json
-    const data = await fs.readFile("db.json", "utf-8");
-    const jsonData = JSON.parse(data);
+    // // Read the content of db.json
+    // const data = await fs.readFile("db.json", "utf-8");
+    // const jsonData = JSON.parse(data);
 
     // Send the JSON data as a response
     res.json(jsonData);
